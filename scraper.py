f2e61d791ee3d57ea147de2b46266c67cf90fa9b
Bogdan Cucu <cucu_bogdan00@yahoo.com>
2026-08-11 17:04:32 +0300

    feat: implement telegram notification system for new articles

--- scraper.py
+++ scraper.py
@@ -2,11 +2,19 @@ import feedparser
 import requests
 import hashlib
 import sqlite3
+import os
 
-
+from dotenv import load_dotenv
 from database import init_db
 
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
 init_db()
+
+new_articles = []
 
 rss_sources = [
         "https://openai.com/news/rss.xml",
@@ -53,6 +61,14 @@ for url in rss_sources:
                     VALUES (?, ?, ?, ?, ?, datetime('now'), ?)
                     """, (article_id, entry.title, entry.link, getattr(entry, 'summary', 'N/A'), getattr(entry, 'published', 'N/A'), url))
 
+                if cursor.rowcount == 1:
+                    new_articles.append(
+                        {
+                            'title' : entry.title,
+                            'link' : entry.link
+                        }
+                    )
+
             connection.commit()   
         else:
             print(f"Failed to fetch {url}, status code: {response.status_code}")
@@ -61,3 +77,26 @@ for url in rss_sources:
         print(f"An error occurred while fetching {url}: {e}")
 
 connection.close()
+
+print(f"Scraping finished. Found {len(new_articles)} new articles.")
+
+if len(new_articles) > 0:
+    print("Sending notifications to Telegram...")
+
+    for article in new_articles:
+        message_text = f"🚨 New AI Article!\n\nTitle: {article['title']}\nLink: {article['link']}"
+
+        url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+        payload = {
+            'chat_id': TELEGRAM_CHAT_ID,
+            'text': message_text
+        }
+
+        try:
+            telegram_response = requests.post(url, json=payload)
+            if telegram_response.status_code != 200:
+                print(f"Failed to send message: {telegram_response.text}")
+        except Exception as e:
+            print(f"Error sending to Telegram: {e}")
+
+print("Process completed successfully.")
